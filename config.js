3c834ff50ee71d9ecd136592f5bccda400f28959
temidevops1 <134103258+temidevops1@users.noreply.github.com>
2023-06-06 10:32:12 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,44 @@
 module.exports = {
-    username: "Temi-reno",
-    gitAuthor: "Temi-reno <339479+temi-reno@users.noreply.github.com>",
-    platform: 'github',
-    onboarding: true, // disables the creation of renovate.json in each repository
-    dependencyDashboard: true,
-    labels: ["renovatebot"],
-    requiredStatusChecks: null,
-    packageRules: [
-        {
-            matchManagers: ['terraform'],
-            matchUpdateTypes: ['minor', 'patch', 'pin'],
-            automerge: true,
-            recreateClosed: true
+    githubActionsConfig: `
+jobs:
+  autoapprove:
+    name: Auto Approve a PR by dependabot
+    runs-on: ubuntu-latest
+    steps:
+      - name: Auto approve
+        uses: hmarr/auto-approve-action@v2.0.0
+        if: github.actor == 'dependabot[bot]' || github.actor == 'dependabot-preview[bot]'
+        with:
+          github-token: ${{ secrets.GITHUB_ACTIONS_TOKEN }}
+    `,
+    renovateConfig: {
+        username: "Temi-reno",
+        gitAuthor: "Temi-reno <339479+temi-reno@users.noreply.github.com>",
+        platform: 'github',
+        onboarding: true,
+        dependencyDashboard: true,
+        labels: ["renovatebot"],
+        requiredStatusChecks: null,
+        packageRules: [
+            {
+                matchManagers: ['terraform'],
+                matchUpdateTypes: ['minor', 'patch', 'pin'],
+                automerge: true,
+                recreateClosed: true
+            },
+            {
+                matchManagers: ['terraform'],
+                matchUpdateTypes: ['major'],
+                automerge: false,
+                recreateClosed: true
+            },
+        ],
+        enabledManagers: ["terraform"],
+        lockFileMaintenance: {
+            enabled: true,
+            automerge: true
         },
-        {
-            matchManagers: ['terraform'],
-            matchUpdateTypes: ['major'],
-            automerge: false,
-            recreateClosed: true
-        },
-    ],
-    enabledManagers: ["terraform"],
-    lockFileMaintenance: {
-        enabled: true,
-        automerge: true
-    },
-    updateLockFiles: true,
-    repositories: ['temidevops1/test-script','temidevops1/documentation-repo'],
+        updateLockFiles: true,
+        repositories: ['temidevops1/test-script','temidevops1/documentation-repo'],
+    }
 };
