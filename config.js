001f4ff8fce1bd8444b13a865cfc8350ddbe8403
temidevops1 <134103258+temidevops1@users.noreply.github.com>
2023-06-06 16:25:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -30,23 +30,5 @@ module.exports = {
   updateLockFiles: true,
   repositories: ['temidevops1/test-script','temidevops1/documentation-repo'],
 };
-name: 'Auto approve and merge PRs by dependabot'
 
-on:
-  pull_request:
-
-jobs:
-  autoapprove:
-    name: 'Auto Approve a PR by dependabot'
-    runs-on: 'ubuntu-latest'
-    steps:
-      - name: 'Auto approve'
-        uses: 'hmarr/auto-approve-action@v2.0.0'
-        if: "github.actor == 'dependabot[bot]' || github.actor == 'dependabot-preview[bot]'"
-        with:
-          github-token: '${{ secrets.GITHUB_ACTIONS_TOKEN }}'
-
-  automerge:
-    name: 'Auto merge after successful checks'
-    needs: 'autoapprove'
 
